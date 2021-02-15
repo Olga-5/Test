ecd9af5143dd1c21d607d4db048e147af9ffd679
Olga Sharapo <olgasharapo2@gmail.com>
2021-02-16 02:52:54 +0300

    feat: add calc for table

--- js/index.js
+++ js/index.js
@@ -19,3 +19,37 @@ table.addEventListener('click', (event) => {
 gridTable.addEventListener('click', (event) => {
   event.stopPropagation();
 });
+
+const cellStartValue = document.querySelector('.start-value');
+const cellPercent = document.querySelector('.percent');
+const cellFirstResult = document.querySelector('.result-first');
+const cellSecondResult = document.querySelector('.result-second');
+const cellThirdResult = document.querySelector('.result-third');
+
+let startValue = 0;
+let percentValue = 0;
+
+const calc = (value, percentValue) => {
+  const persent = value * percentValue / 100;
+  return value + persent;
+}
+
+const changeCellsData = () => {
+  const firstResult = calc(startValue, percentValue);
+  const secondResult = calc(firstResult, percentValue);
+  const thirdResult = calc(secondResult, percentValue);
+
+  cellFirstResult.innerHTML = firstResult;
+  cellSecondResult.innerHTML = secondResult;
+  cellThirdResult.innerHTML = thirdResult;
+}
+
+cellStartValue.addEventListener('input', (event) => {
+  startValue = event.target.value === '' ? 0 : parseFloat(event.target.value);
+  changeCellsData();
+});
+
+cellPercent.addEventListener('input', (event) => {
+  percentValue = event.target.value === '' ? 0 : parseFloat(event.target.value);
+  changeCellsData();
+});
