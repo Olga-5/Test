27a13d812773b6bb57cdeb6da1e8116b24380881
Olga Sharapo <olgasharapo2@gmail.com>
2021-02-15 18:10:36 +0300

    feat: add table with grid

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const toggle = document.querySelector('.content-toggle');
 const overlay = document.querySelector('.main-overlay');
 const content = document.querySelector('.main-content');
 const table = document.querySelector('.table');
+const gridTable = document.querySelector('.grid-table');
 
 toggle.addEventListener('click', () => {
   overlay.classList.add('hide');
@@ -9,8 +10,12 @@ toggle.addEventListener('click', () => {
 
 content.addEventListener('click', () => {
   overlay.classList.remove('hide');
-})
+});
 
 table.addEventListener('click', (event) => {
   event.stopPropagation();
-})
+});
+
+gridTable.addEventListener('click', (event) => {
+  event.stopPropagation();
+});
