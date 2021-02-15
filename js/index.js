a17c861d5d92ff9cb80e82b661cf0bbfb32ad4fe
Olga Sharapo <olgasharapo2@gmail.com>
2021-02-15 16:06:25 +0300

    feat: slide 2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
-document.fonts.ready.then(function() {
-  console.log('hi')
+const toggle = document.querySelector('.content-toggle');
+const overlay = document.querySelector('.main-overlay');
+const content = document.querySelector('.main-content');
+const table = document.querySelector('.table');
+
+toggle.addEventListener('click', () => {
+  overlay.classList.add('hide');
 });
+
+content.addEventListener('click', () => {
+  overlay.classList.remove('hide');
+})
+
+table.addEventListener('click', (event) => {
+  event.stopPropagation();
+})
